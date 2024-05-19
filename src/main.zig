df9584cccbd8a70cc341453b90d6bc94a377765f
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2024-05-19 09:13:47 -0700

    remove falsify() 'arg_types' param
    this way you don't have to specify the arg types twice when calling
    falsify().  now pred()'s first param type is used instead.
    
    * use struct tuples instead of meta.ArgsTuple()
    * simplify things by inlining some functions

--- src/main.zig
+++ src/main.zig
@@ -3,66 +3,49 @@ const zigthesis = @import("zigthesis.zig");
 const utils = @import("utils.zig");
 
 pub fn main() void {
-    zigthesis.falsify(
-        struct {
-            pub fn pred(args: std.meta.Tuple(&.{ i32, i32, i32 })) bool {
-                const x = args[0];
-                const y = args[1];
-                const z = args[2];
+    zigthesis.falsify(struct {
+        pub fn pred(args: struct { i32, i32, i32 }) bool {
+            const x = args[0];
+            const y = args[1];
+            const z = args[2];
 
-                return (x + y) * z == x * (y + z);
-            }
-        }.pred,
-        &[_]type{ i32, i32, i32 },
-        "Example 1"
-    );
+            return (x + y) * z == x * (y + z);
+        }
+    }.pred, "Example 1");
 
-    zigthesis.falsify(
-        struct {
-            pub fn pred(args: std.meta.Tuple(&.{ i32, i32 })) bool {
-                const x = args[0];
-		const y = args[1];
+    zigthesis.falsify(struct {
+        pub fn pred(args: struct { i32, i32 }) bool {
+            const x = args[0];
+            const y = args[1];
 
-                return x*y == y*x;
-            }
-        }.pred,
-        &[_]type{ i32, i32 },
-        "Commutative Property of Multiplication (Integers)"
-    );
-    zigthesis.falsify(
-        struct {
-            pub fn pred(args: std.meta.Tuple(&.{ i32, i32 })) bool {
-                const x = args[0];
-                const y = args[1];
+            return x * y == y * x;
+        }
+    }.pred, "Commutative Property of Multiplication (Integers)");
 
-                return x-y == y-x;
-            }
-        }.pred,
-        &[_]type{ i32, i32 },
-        "Commutative Property of Subtraction (Integers)" 
-    );
-    zigthesis.falsify(
-        struct {
-            pub fn pred(args: std.meta.Tuple(&.{ f32, f32, f32 })) bool {
-                const x = args[0];
-                const y = args[1];
-                const z = args[2];
+    zigthesis.falsify(struct {
+        pub fn pred(args: struct { i32, i32 }) bool {
+            const x = args[0];
+            const y = args[1];
 
-                return (x + y) + z == x + (y + z);
-            }
-        }.pred,
-        &[_]type{ f32, f32, f32},
-        "Associativity of Floats"
-    );
-    zigthesis.falsify(
-        struct {
-            pub fn pred(args: std.meta.Tuple(&.{[3]i32})) bool {
-                const x = args[0];
+            return x - y == y - x;
+        }
+    }.pred, "Commutative Property of Subtraction (Integers)");
 
-               return utils.sum(x[0..]) < 100;
-            }
-        }.pred,
-        &[_]type{[3]i32},
-        "Sum of Elements in List"
-    );
+    zigthesis.falsify(struct {
+        pub fn pred(args: struct { f32, f32, f32 }) bool {
+            const x = args[0];
+            const y = args[1];
+            const z = args[2];
+
+            return (x + y) + z == x + (y + z);
+        }
+    }.pred, "Associativity of Floats");
+
+    zigthesis.falsify(struct {
+        pub fn pred(args: struct { [3]i32 }) bool {
+            const x = args[0];
+
+            return utils.sum(x[0..]) < 100;
+        }
+    }.pred, "Sum of Elements in List");
 }
