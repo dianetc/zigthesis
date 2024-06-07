04855b83c2d4f62616b6b879f6b5bf9caa44e8d7
dianetc <dtchuindjo@gmail.com>
2024-06-07 07:38:39 -0400

    trying out non-crypto library

--- src/generate.zig
+++ src/generate.zig
@@ -4,36 +4,37 @@ const std = @import("std");
 const A = 65;
 const z = 122;
 
+
 pub fn Generator(comptime Predicate: type) type {
     const Arg = @typeInfo(Predicate).Fn.params[0].type.?;
 
     return struct {
-        pub fn generate() Arg {
+        pub fn generate(random: std.rand.Random) Arg {
             var arg: Arg = undefined;
             inline for (@typeInfo(Arg).Struct.fields) |field| {
-                @field(arg, field.name) = generateField(field.type);
+                @field(arg, field.name) = generateField(random, field.type);
             }
             return arg;
         }
     };
 }
 
-fn generateField(comptime T: type) T {
+fn generateField(random: std.rand.Random, comptime T: type) T {  
     switch (@typeInfo(T)) {
         .Int => {
             if (T == u8){
-                return std.crypto.random.intRangeAtMost(T, A, z);
+                return random.intRangeAtMost(T, A, z);
             } else {
-                return std.crypto.random.intRangeAtMost(T, std.math.pow(i32, -10, 3), std.math.pow(i32, 10, 3));
+                return random.intRangeAtMost(T, std.math.pow(i32, -10, 3), std.math.pow(i32, 10, 3));
             }
         },
         .Float => {
-            return std.crypto.random.float(T);
+            return random.float(T);
         },
         .Array => |array_info| {
             var array: T = undefined;
             for (&array) |*item| {
-                    item.* = generateField(array_info.child);
+                    item.* = generateField(random, array_info.child);
             }
             return array;
         },
--- src/zigthesis.zig
+++ src/zigthesis.zig
@@ -3,9 +3,20 @@ const generate = @import("generate.zig");
 
 const MAX_DURATION_MS: u64 = 10 * 1000;
 
-pub fn falsify(predicate: anytype, test_name: []const u8) void {
+
+pub fn getRandom() !std.rand.Random {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    return prng.random();
+}
+
+pub fn falsify(predicate: anytype, test_name: []const u8) !void {
     const gen = generate.Generator(@TypeOf(predicate));
     const start_time = std.time.milliTimestamp();
+    const random = try getRandom();
     while (true) {
         const current_time = std.time.milliTimestamp();
         if (current_time - start_time >= MAX_DURATION_MS) {
@@ -13,7 +24,7 @@ pub fn falsify(predicate: anytype, test_name: []const u8) void {
             return;
         }
 
-        const args = gen.generate();
+        const args = gen.generate(random);
         const result = predicate(args);
         if (!result) {
             std.debug.print("{s} failed with case: {any}\n", .{ test_name, args });
